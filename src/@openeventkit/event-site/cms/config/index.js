0ac05cd01603a1f3e62154c31e783e4e340fcda7
Gabriel Horacio Cutrini <gabriel.cutrini@gmail.com>
2023-05-05 18:43:04 -0300

    Sets NetlifyCMS backend repo config to fntechgit/fnevent-roblox-theme

--- src/@openeventkit/event-site/cms/config/index.js
+++ src/@openeventkit/event-site/cms/config/index.js
@@ -7,6 +7,8 @@ export const collections = defaultCollections.concat([
 ]);
 
 const config = structuredClone(defaultConfig);
+// TODO: move to env var
+config.backend.repo = "fntechgit/fnevent-roblox-theme";
 config.collections = collections;
 
 export default config;
