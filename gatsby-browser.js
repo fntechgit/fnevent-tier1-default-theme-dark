e9c9e7f50e17ba651c78575d6358fcf76f2b9a9b
gcutrini <gabriel.cutrini@gmail.com>
2023-05-18 19:07:19 -0300

    Merge pull request #4 from fntechgit/hotfix/client-load-site-colors
    Load marketing api set colors into document styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,14 @@
 import wrapThemeProvider from "./src/utils/wrapThemeProvider";
 
+import colors from "./src/content/colors.json";
+
 export const wrapRootElement = wrapThemeProvider;
+
+export const onClientEntry = () => {
+  // var set at document level
+  // prevents widget color flashing from defaults to fetched by widget from marketing api
+  Object.entries(colors).forEach(color => {
+    document.documentElement.style.setProperty(`--${color[0]}`, color[1]);
+    document.documentElement.style.setProperty(`--${color[0]}50`, `${color[1]}50`);
+  });
+};
