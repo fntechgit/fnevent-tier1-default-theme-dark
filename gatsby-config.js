f71872feece8696553be22b4afbd5737fcd9fc6d
Gabriel Horacio Cutrini <gabriel.cutrini@gmail.com>
2023-06-12 18:32:19 -0700

    Removes theme site metadata re declaration crippling base functionality.

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,6 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  /*flags: {
-    DEV_SSR: true
-  },*/
-  siteMetadata: {
-    title: "RDC 2023",
-    siteUrl: "https://rdc23.fnvirtual.app",
-  },
   plugins: [
     "@openeventkit/event-site"
   ]
