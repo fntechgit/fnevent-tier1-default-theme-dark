1b446f83fafedeb4d237ea5661b66cb477055cf1
Zachary Nespral <ZacNespral21@users.noreply.github.com>
2023-08-03 10:49:32 -0700

    Feature/page header shadow 1col (#12)
    * New PageHeader component
    
    * Styling enchancements for new pageheader
    
    * Added proptypes & check to bg img variable

---
+++ src/@openeventkit/event-site/components/PageHeader/index.js
@@ -0,0 +1,21 @@
+import * as React from "react";
+import PropTypes from "prop-types"
+
+import styles from "./index.module.scss";
+
+const PageHeader = (props) => (
+  <section className={styles.pageHeader} style={ props.backgroundImageSrc && {backgroundImage: `url(${props.backgroundImageSrc})`}}>
+    <div className={styles.titles}>
+      <h1>{props.title}</h1><br/>
+      <h1 className={styles.subtitle}>{props.subtitle}</h1>
+    </div>
+  </section>
+);
+
+PageHeader.propTypes = {
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  backgroundImageSrc: PropTypes.string
+}
+
+export default PageHeader;
