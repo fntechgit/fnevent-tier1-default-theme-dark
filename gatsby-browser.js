cc86cf9b1124477c9fc0f27767ed12aee4ec4bde
sebastian marcet <smarcet@gmail.com>
2023-08-18 16:05:01 -0300

    Feature/dark mode theme (#22)
    * Added dark mode event-site
    
    * Updated event site to v2.0.5
    
    * * updated event-site to 2.0.6
    * fixed css override
    
    * Updated event site to 2.0.7
    
    * fixed lobby page header image
    
    * update event-site to 2.0.9
    
    * updated widgets
    * fixed styles
    
    * Updated event site to 2.0.11
    
    * updated widgets:
      lite schedule
      live evntt
    * updated styles for extra questions DDL
    
    * Updated event-site to 2.0.12
    
    * fixed auth issues
    * fixed not closing dlg on OTP login
    * fixed disqus styles
    
    * Changed css & removed br el
    
    * Updated event-site to 2.0.13
    
    * updated uicore version
    
    * updated event site 2.0.14
    
    * updated a2a widget dark mode
    
    * updated to event-site 2.0.16
    * fixes on a2a widget
    
    * updated event-site 2.0.17
    * css fixes
    
    * Fixed CSS
    
    * Styling fix for lobby page header
    
    * Top margin fix for lobby title
    
    * updated event-site 2.0.18
    * css fixes for a2a
    
    * updated event-site 2.0.19
    
    * css fixes
    
    * * updated event site
    * css tweaks
    
    * updated event-site 2.0.22
    
    * updated event-site 2.0.24
    
    * reg lite
    
    * Updated event site 2.0.25
    
    * updated event site 2.0.26
    
    * Updated event site 2.0.27
    
    * Updated event site 2.0.28
    
    * Updated event site 2.0.33
    
    * [CUSTOM] Change font family to Builder Sans (#18)
    
    * Add custom font to rdc site
    
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>
    
    * Change style files
    
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>
    
    * Change style and browser files
    
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>
    
    * update font location file
    
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>
    
    * Fix font file src
    
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>
    
    ---------
    
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>
    
    * updated event site 2.0.34
    
    * added mobile styling fixes with new PR from dev (#19)
    
    * Apply css variables for filter button (#20)
    
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>
    
    * updated event-site 2.0.35
    
    * updated event-site 2.0.36
    
    * Adjusting color for filter button (#21)
    
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>
    
    ---------
    
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>
    Co-authored-by: Zachary Nespral <zacharynespral@gmail.com>
    Co-authored-by: Tomás Castillo <tomrndom@users.noreply.github.com>
    Co-authored-by: Zachary Nespral <ZacNespral21@users.noreply.github.com>

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,5 @@
 import wrapThemeProvider from "./src/utils/wrapThemeProvider";
 
+import "./src/styles/fonts.scss";
+
 export const wrapRootElement = wrapThemeProvider;
--- src/@openeventkit/event-site/components/PageHeader/index.js
+++ src/@openeventkit/event-site/components/PageHeader/index.js
@@ -6,7 +6,7 @@ import styles from "./index.module.scss";
 const PageHeader = (props) => (
   <section className={styles.pageHeader} style={ props.backgroundImageSrc && {backgroundImage: `url(${props.backgroundImageSrc})`}}>
     <div className={styles.titles}>
-      <h1>{props.title}</h1><br/>
+      <h1>{props.title}</h1>
       <h1 className={styles.subtitle}>{props.subtitle}</h1>
     </div>
   </section>
